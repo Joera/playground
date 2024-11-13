680765157b30665dd6832e694404e6d6717c38e7
Joera <joera@joeramulders.com>
2024-11-13 22:08:20 +0100

    rescue

--- src/routes/avatar/scan/+page.svelte
+++ src/routes/avatar/scan/+page.svelte
@@ -1,17 +1,33 @@
 <script lang="ts">
     import { BarcodeScanner } from 'svelte-barcode-scanner';
+    import { writable } from 'svelte/store';
 
     let previewWidth;
     let mediaErrorMessage = "";
+
+    const newby = writable({});
   
     function onQRScan(event: CustomEvent) {
-      alert(event.detail.qrContent);
+      
+      newby.update((n) => {
+        n = event.detail;
+        return n
+      })
     }
 </script>
  
-<div class="barcode-scanner">
-	<BarcodeScanner />
-</div>
+<article>
+
+  <div>{JSON.stringify($newby)}</div>
+
+
+  {#if Object.keys($newby).length == 0}
+  <div class="barcode-scanner">
+    <BarcodeScanner />
+  </div>
+  {/if}
+
+</article>
 
 <style>
 	.barcode-scanner {
@@ -19,4 +35,12 @@
 		max-width: 384px;
 		aspect-ratio: 1;
 	}
+
+  article {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;  
+  }
 </style>
