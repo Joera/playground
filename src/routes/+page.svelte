9b12c8faa4e45df9c998ef755717a61507b7be44
Joera <joera@joeramulders.com>
2024-11-13 11:23:33 +0100

    progress

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,38 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import SafeOverview from '$lib/components/SafeOverview.svelte';
+    import { safe_store, addSafe } from '$lib/safe.store';
+
+    $: safeEntries = Object.entries($safe_store);
+
+    const handleAddSafe = async () => {
+            addSafe();
+    }
+
+</script>
+
+<style>
+    #safes {
+        display: flex;
+        flex-direction: row;
+        justify-content: flex-start;
+        align-items: center;
+        width: calc(100vw - 1rem);
+        min-height: calc(100vh - 6rem);
+        overflow: auto;
+        margin: 0 .5rem;
+    }
+
+
+</style>
+
+{#if $safe_store && Object.keys($safe_store).length > 0}
+    <section id="safes">
+        {#each safeEntries as [safe_address, safeSrv]}        
+            <SafeOverview {safe_address} {safeSrv} />
+        {/each}
+        <article class="safe_container">
+            <button on:click={handleAddSafe}>+</button>
+        </article>
+    </section>
+  
+{/if}
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,32 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import NodeGlobalsPolyfillPlugin from 'vite-plugin-node-stdlib-browser';
+
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(), 
+		NodeGlobalsPolyfillPlugin({
+			process: true,
+			buffer: true
+		}), 
+	],
+	define: {
+		global: 'window', 
+		'process.env': {},
+	},
+	resolve: {
+		alias: {
+			stream: 'stream-browserify',
+            crypto: 'crypto-browserify',
+            http: 'stream-http',
+            https: 'https-browserify',
+            os: 'os-browserify/browser',
+            url: 'url',
+			'es6-promise': 'es6-promise/auto'
+		}
+	},
+	optimizeDeps: {
+		include: ['buffer', 'stream', 'crypto']
+	}
 });
