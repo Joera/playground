197cb29263324c733ee6020d4fc5b4adee0113a9
Joera <joera@joeramulders.com>
2024-12-18 17:37:12 +0100

    base

---
+++ src/lib/components/IconScanner.svelte
@@ -0,0 +1,13 @@
+<svg id="scanner" xmlns="http://www.w3.org/2000/svg" data-name="Layer 1" viewBox="0 0 100 125" x="0px" y="0px" fill="#fff" width="40" height="48"><title>Artboard 34</title><path d="M71,22H61v6H71a1,1,0,0,1,1,1V39h6V29A7,7,0,0,0,71,22Z"/><path d="M39,22H29a7,7,0,0,0-7,7V39h6V29a1,1,0,0,1,1-1H39Z"/><path d="M29,78H39V72H29a1,1,0,0,1-1-1V61H22V71A7,7,0,0,0,29,78Z"/><path d="M72,71a1,1,0,0,1-1,1H61v6H71a7,7,0,0,0,7-7V61H72Z"/><rect x="47" y="47" width="6" height="6"/><rect x="55" y="39" width="6" height="6"/><rect x="39" y="39" width="6" height="6"/><rect x="55" y="55" width="6" height="6"/><rect x="39" y="55" width="6" height="6"/></svg>
+
+<style>
+     #scanner {
+
+    margin-left: auto;
+    /* margin-right: 1rem; */
+    margin-top: 9px;
+    position: absolute;
+    right: 1rem;
+
+    }
+</style>
